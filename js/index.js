498cacac828ea147440a4da07b659126a582166a
cth3403 <jschulkins@gmail.com>
2018-08-16 22:02:43 +0100

    initial commit

---
+++ js/index.js
@@ -0,0 +1,159 @@
+var plays = {"plays":
+             [
+             	{
+	"play": "A Midsummer Nights Dream",
+	"characters": [
+			{"character":"Puck", "acts":
+				[
+					{"act":3,"scene":2, "quote_pd":"Cupid is a knavish lad, |Thus to make poor females mad. |", "quote_nd": "Cupid is a knavish lad, Thus to make poor females mad."},
+					{"act":3,"scene":2, "quote_pd":"Jack shall have Jill, |Nought shall go ill, |The man shall have his mare again, and all shall be well. |", "quote_nd": "Jack shall have Jill, Nought shall go ill, The man shall have his mare again, and all shall be well."},
+					{"act":5,"scene":1,"quote_pd":"...not a mouse |Shall disturb this hallowed house. |I am sent with broom before, |To sweep the dust behind the door. |", "quote_nd":"...not a mouse Shall disturb this hallowed house. I am sent with broom before, To sweep the dust behind the door."},
+					{"act":5,"scene":1,"quote_pd":"If we shadows have offended, |Think but this, and all is mended, |That you have but slumbered here |While these visions did appear. |", "quote_nd":"If we shadows have offended, Think but this, and all is mended, That you have but slumbered here While these visions did appear."}
+				] 
+			},
+			{"character":"Bottom", "acts":
+				[
+					{"act":3, "scene":1, "quote_pd":"To say the truth, |reason and love keep little |company together nowadays. |", "quote_nd":"To say the truth, reason and love keep little company together nowadays."},
+					{"act":4, "scene":1, "quote_pd":"I have had a most rare vision. |I had a dream, past the wit of man to say what dream it was… |The eye of man hath not heard, |the ear of man hath not seen, man's hand is not able to taste, |his tongue to conceive, |nor his heart to report, |what my dream was. |", "quote_nd":"I have had a most rare vision. I had a dream, past the wit of man to say what dream it was… The eye of man hath not heard, the ear of man hath not seen, man's hand is not able to taste, his tongue to conceive, nor his heart to report, what my dream was."}
+				]
+			},
+			{"character":"Helena", "acts":
+				[
+					{"act":1, "scene":1, "quote_pd":"Love looks not with the eyes, but with the mind, |And therefore is winged Cupid painted blind. |", "quote_nd":"Love looks not with the eyes, but with the mind, And therefore is winged Cupid painted blind."},
+					{"act":3, "scene":2, "quote_pd":"O, when she's angry, she is keen and shrewd. |She was a vixen when she went to school, |And though she be but little, she is fierce |", "quote_nd":"O, when she's angry, she is keen and shrewd. She was a vixen when she went to school, And though she be but little, she is fierce"}
+				]
+			},
+			{"character":"Titania", "acts":
+				[
+					{"act":3, "scene":1, "quote_pd":"What angel wakes me from my flow’ry bed? |", "quote_nd":"What angel wakes me from my flow’ry bed?"},
+					{"act":4, "scene":1, "quote_pd":"Methought I was enamoured of an ass. |", "quote_nd":"Methought I was enamoured of an ass."}
+				]
+			},
+			{"character":"Lysander", "acts":
+				[
+					{"act":1, "scene":1, "quote_pd":"The course of true love never did run smooth. |", "quote_nd":"The course of true love never did run smooth."}
+				]
+			},
+			{"character":"Oberon", "acts":
+				[
+					{"act":2, "scene":1, "quote_pd":"Ill met by moonlight, proud Titania. |", "quote_nd":"Ill met by moonlight, proud Titania."},
+					{"act":2, "scene":1, "quote_pd":"I know a bank where the wild thyme blows, |Where oxlips and the nodding violet grows, |Quite over-canopied with luscious woodbine, |With sweet musk-roses and |with eglantine: |There sleeps Titania sometime of the night, |Lulled in these flowers with dances and delight. |", "quote_nd":"I know a bank where the wild thyme blows, Where oxlips and the nodding violet grows, Quite over-canopied with luscious woodbine, With sweet musk-roses and with eglantine: There sleeps Titania sometime of the night, Lulled in these flowers with dances and delight. ."}
+				]
+			},
+			{"character":"Quince", "acts":
+				[
+					{"act":3, "scene":1, "quote_pd":"Bless thee, Bottom! Bless thee! Thou art translated. |", "quote_nd":"Bless thee, Bottom! Bless thee! Thou art translated."}
+				]
+			}
+		]	
+	},
+	{
+	"play": "Twelfth Night",
+	"characters": [
+			{"character":"Orsino", "acts":
+				[
+					{"act":1, "scene":1, "quote_pd":"If music be the food of love, play on.|", "quote_nd": "If music be the food of love, play on."},
+					{"act":5, "scene":1, "quote_pd":"One face, one voice, one habit, and two persons, |A natural perspective that is and is not!|", "quote_nd":"One face, one voice, one habit, and two persons, A natural perspective that is and is not!"},
+					{"act":5, "scene":1, "quote_pd":"Cesario, come – |For so you shall be, while you are a man. |But when in other habits you are seen, |Orsino's mistress, and his fancy's queen.|","quote_nd":"Cesario, come – For so you shall be, while you are a man. But when in other habits you are seen, Orsino's mistress, and his fancy's queen."}
+				]
+			},
+			{"character":"Viola", "acts":
+				[
+					{"act":1, "scene":2, "quote_pd":"What country, friends, is this?|", "quote_nd": "What country, friends, is this?"},
+					{"act":1, "scene":4, "quote_pd":"I'll do my best |To woo your lady. – Yet, a barful strife! |Whoe'er I woo, myself would be his wife.|", "quote_nd":"I'll do my best To woo your lady. – Yet, a barful strife! Whoe'er I woo, myself would be his wife."},
+					{"act":1, "scene":5, "quote_pd":"Make me a willow cabin at your gate, |And call upon my soul within the house.|","quote_nd":"Make me a willow cabin at your gate, And call upon my soul within the house."},
+					{"act":2, "scene":2, "quote_pd":"O time, thou must untangle this, not I. |It is too hard a knot for me t'untie.|","quote_nd":"O time, thou must untangle this, not I. It is too hard a knot for me t'untie."},
+					{"act":2, "scene":4, "quote_pd":"She pined in thought, |And with a green and yellow melancholy |She sat like patience on a monument, |Smiling at grief.|","quote_nd":"She pined in thought, And with a green and yellow melancholy She sat like patience on a monument, Smiling at grief."},
+					{"act":3, "scene":4, "quote_pd":"Prove true, imagination, O, prove true, |That I, dear brother, be now ta'en for you!|","quote_nd":"Prove true, imagination, O, prove true, That I, dear brother, be now ta'en for you!"}
+				]
+			},
+			{"character":"Sir Andrew", "acts":
+				[
+					{"act":1, "scene":3, "quote_pd":"But I am a great eater of beef, and I believe that does harm to my wit.|", "quote_nd": "But I am a great eater of beef, and I believe that does harm to my wit."},
+					{"act":2, "scene":3, "quote_pd":"I was adored once too.|", "quote_nd":"I was adored once too."}
+				]
+			},
+			{"character":"Feste", "acts":
+				[
+					{"act":1, "scene":5, "quote_pd":"Many a good hanging prevents a bad marriage.|", "quote_nd": "Many a good hanging prevents a bad marriage."},
+					{"act":1, "scene":5, "quote_pd":"Better a witty fool than a foolish wit.|", "quote_nd":"Better a witty fool than a foolish wit."},
+					{"act":3, "scene":1, "quote_pd":"Foolery, sir, does walk about the orb like the sun, it shines everywhere.|", "quote_nd":"Foolery, sir, does walk about the orb like the sun, it shines everywhere."},
+					{"act":5, "scene":1, "quote_pd":"And thus the whirligig of time brings in his revenges.|", "quote_nd":"And thus the whirligig of time brings in his revenges."}
+				]
+			},
+			{"character":"Olivia", "acts":
+				[
+					{"act":1, "scene":5, "quote_pd":"How now? |Even so quickly may one catch the plague?|", "quote_nd": "How now? Even so quickly may one catch the plague?"}
+				]
+			},
+			{"character":"Sir Toby", "acts":
+				[
+					{"act":2, "scene":3, "quote_pd":"Dost thou think, because thou art virtuous, there shall be no more cakes and ale?|", "quote_nd": "Dost thou think, because thou art virtuous, there shall be no more cakes and ale?"}
+				]
+			},
+			{"character":"Malvolio", "acts":
+				[
+					{"act":2, "scene":5, "quote_pd":"Some are born great, some achieve greatness and some have greatness thrust upon 'em.|", "quote_nd": "Some are born great, some achieve greatness and some have greatness thrust upon 'em."},
+					{"act":5, "scene":1, "quote_pd":"I’ll be revenged on the whole pack of you.|", "quote_nd": "I’ll be revenged on the whole pack of you."}
+				]
+			},
+			{"character":"Fabian", "acts":
+				[
+					{"act":3, "scene":4, "quote_pd":"If this were played upon a stage now, I could condemn it as an improbable fiction.|", "quote_nd": "If this were played upon a stage now, I could condemn it as an improbable fiction."}
+				]
+			}
+		]	
+	}
+	]
+};
+
+function random_item(items){
+  return items[Math.floor(Math.random()*items.length)];
+}
+
+$(".random > a").click(function(){
+  $("#tweet-container").empty();
+  var play = random_item(plays.plays);
+  var characters = random_item(play.characters);
+  var acts = random_item(characters.acts);
+  var quote = acts.quote_nd;
+  play = play.play;
+  characters = characters.character;
+  acts = "Act: "+acts.act+" Scene: "+acts.scene;
+  
+ // var play = $(this).text();
+  //$("#play_src").text(play);
+  
+  var twitter="https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=";
+  
+ /* $.ajax({
+  method: "GET",
+  url: str
+})
+  .done(function( quote ) {
+   
+  var url = encodeURI(quote); 
+  */
+  $("#quote_well").removeClass('hidden');
+  $("#text_q").text(quote);
+  $(".character").text(characters);
+  $(".act").text(acts);
+   $(".play").text(play);
+  var tweet_str = '"'+quote+'"\r\n'+characters+'\r\n'+acts+"\r\n"+play+"\r\n";
+    
+   twttr.widgets.createShareButton(
+  "https:\/\/codepen.io\/cth3403\/full/PjJyGX",
+  document.getElementById("tweet-container"),
+  {
+    size: "large",
+    text: tweet_str,
+    hashtags: "quotes",
+    related: "freecodecamp"
+  }
+);
+  
+//  });
+  
+  
+  
+});
